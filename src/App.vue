7e57fe52836c093e6c705dc8c2575ed4f784f1b0
cz <3302076969@qq.com>
2024-07-10 17:13:57 +0800

    ADD全局项目入口，有全局单次调用的代码，都可以写在这里

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,21 @@ import BasicLayout from "@/layouts/BasicLayout";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 import ACCESS_ENUM from "@/access/accessEnum";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
+/**
+ * 该方法可以用于前端页面初始化
+ * 在前端页面初始化的时候会去调用我们上面所定义的函数
+ */
+onMounted(() => {
+  doInit();
+});
 //获取路由信息
 const router = useRouter();
 //获取当前登录用户状态信息
