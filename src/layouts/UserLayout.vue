df630c57ff92656fd9443a8a0fa0374b6d8d8d0e
cz <3302076969@qq.com>
2024-07-11 16:55:51 +0800

    ADD多套布局根据路由切换

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,43 @@
+//该类表示项目的通用布局
+<template>
+  <div id="userLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header"> 用户布局</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.4399.com" target="_blank">
+          oj在线判题系统 by 程序员陈爱国
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#userLayout {
+}
+
+#userLayout .header {
+  margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#userLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#userLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts"></script>
